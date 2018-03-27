3c4a05601da3a9a7532a9b8df870f98027c2904d
Loris Parata <33716296+LorisParata98@users.noreply.github.com>
2018-03-27 13:03:45 +0200

    a
    a

---
+++ getAuthors.php
@@ -0,0 +1,15 @@
+<?php
+  include("db_info.php");
+  /*Suggestion for the autors*/
+  $sugg = $_GET["sugg"];
+  $conn->query("SET NAMES 'utf8'");
+  $sql = "SELECT name
+          FROM author
+          WHERE name LIKE '%".$sugg."%'";
+  $result = $conn->query($sql);
+  $options = array();
+  while($data = mysqli_fetch_array($result)) {
+    array_push($options, $data['name']);
+  }
+  echo json_encode($options);
+?>
