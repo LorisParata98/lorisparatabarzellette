6157f114e7e136f5c9fc7ed48d31e8f5174186b2
Loris Parata <33716296+LorisParata98@users.noreply.github.com>
2018-03-27 12:42:14 +0200

    a
    a

--- categories.php
+++ categories.php
@@ -74,15 +74,11 @@
                     AND j.idjoke = jc.idjoke
                     AND jc.idcategory = c.idcategory";
 
-            if($result = $conn->query($sql)) {
-                while ($row = $result->fetch_assoc()) {
-                   
-                    echo "<div class='elenco'>";
-        echo "<table>";
-        $likesconta = 0;}
-     while($row = $result->fetch_assoc())
-     {
-        
+        if($result = $conn->query($sql)) {
+             while ($row = $result->fetch_assoc()) {
+                   echo "<div class='elenco'>";
+                   echo "<table>";
+        $likesconta = 0;
          echo  "<tr><td style='border:5px solid rgb(".
          rand(0,255).",".rand(0,255).",".rand(0,255).
          ")' >". $row["joketext"] . "<br>" ."Autore : " . $row["name"]  . "<div name='div_likes".$likesconta."'style='text-align: right'>Likes :  " . $row["likes"]. "</div>" ."Data : ". $row["jokedate"] . "<br>";        
